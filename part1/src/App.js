c9a6fb45a31803e826cf418991dcf9c0c7f0e7ff
fibon-web <grzegorzadamczyk01093#gmail.com>
2022-11-27 12:35:36 +0100

    Initialize

---
+++ part1/src/App.js
@@ -0,0 +1,54 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.name} {props.exercises}</p>
+  );
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part name={props.parts[0].name} exercises={props.parts[0].exercises}/>
+      <Part name={props.parts[1].name} exercises={props.parts[1].exercises}/>
+      <Part name={props.parts[2].name} exercises={props.parts[2].exercises}/>
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.total}</p>
+  )
+}
+
+export default function App() {
+    const course = {
+      name: "Half Stack application development",
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7
+        },
+        {
+          name: "State of a component",
+          exercises: 14
+        },
+      ]
+    }
+    
+    return (
+      <div>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total total={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}/>
+    </div>
+  );
+}
+  
